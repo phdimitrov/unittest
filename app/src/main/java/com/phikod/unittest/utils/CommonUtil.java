c4cc940874a3dbe9cf91b9e489df5d7dcd6a72e9
Philip Dimitrov <ph.dimitrov@gmail.com>
2017-07-21 18:42:43 +0300

    jacocotest

---
+++ app/src/main/java/com/phikod/unittest/utils/CommonUtil.java
@@ -0,0 +1,13 @@
+package com.phikod.unittest.utils;
+
+/**
+ * @author Philip
+ * @since 2017-07-20
+ */
+
+public class CommonUtil {
+
+    public static int addition(int x, int y) {
+        return x + y;
+    }
+}
--- app/src/test/java/com/phikod/unittest/ExampleUnitTest.java
+++ app/src/test/java/com/phikod/unittest/ExampleUnitTest.java
@@ -1,5 +1,11 @@
 package com.phikod.unittest;
 
+import android.content.Context;
+import android.provider.Settings;
+
+import com.phikod.unittest.utils.CommonUtil;
+
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -10,8 +16,20 @@ import static org.junit.Assert.*;
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 public class ExampleUnitTest {
+
+    Context mContext;
+
+    @Before
+    public void initialize() {
+        System.out.println("initialize");
+    }
+
     @Test
     public void addition_isCorrect() throws Exception {
-        assertEquals(4, 2 + 2);
+        System.out.println("addition_isCorrect");
+        CommonUtil util = new CommonUtil();
+        int result = CommonUtil.addition(2, 2);
+
+        assertEquals(4, result);
     }
 }
